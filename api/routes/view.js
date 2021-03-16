e64b22abd8e26c0e83f5298053abcca80d7ba3c5
Tunç Gürsoy <tuncgursoy@hotmail.com>
2021-03-16 21:10:09 +0300

    View Route Update

--- api/routes/view.js
+++ api/routes/view.js
@@ -11,16 +11,6 @@ router.get('/create',function(req, res)
 {
     res.render('create');
 }); 
-
-router.get('/delete',function(req,res)
-{
-    res.render('delete');
-});
-
-router.get('/update',function(req,res)
-{
-    res.render('update');
-});
 router.get('/read',function(req,res)
 {
     res.redirect('/viewsapi/read');
